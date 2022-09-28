d596b12c3b99ba78c136400a8e10f04699c8d83d
Kollurupranathi <kpranathi009@gmail.com>
2022-09-28 19:34:23 +0530

    changes made

--- StudentList.java
+++ StudentList.java
@@ -5,11 +5,9 @@ class Student{
     
     //Setter methods
     public void setName(String name1){studentName=name1;}
-    public void setScores(int score1,int score2,int score3){
-        test1=score1;
-        test2=score2;
-        test3=score3;
-    }
+    public void setTest1Score(int score1){test1=score1;}
+    public void setTest2Score(int score2){test2=score2;}
+    public void setTest3Score(int score3){test3=score3;}
 
     //methods
     public double average() { // To compute average test grade
@@ -35,7 +33,9 @@ public class StudentList {
                 else if(mystr.matches("^[ A-Za-z]+$")){std1.setName(mystr);}
                 else{
                     String[] arr = mystr.split(", ", 0);
-                    std1.setScores(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
+                    std1.setTest1Score(Integer.parseInt(arr[0])); 
+                    std1.setTest2Score(Integer.parseInt(arr[1]));
+                    std1.setTest3Score(Integer.parseInt(arr[2]));
                 }
         }
         in.close();
